f16711fdf57d209edf4929b26517d22e4320ca98
gaponov <wmconstructor@gmail.com>
2023-01-29 21:27:01 +0200

    Delete Spring Data

--- src/main/java/ua/gaponov/todoApp/entities/Note.java
+++ src/main/java/ua/gaponov/todoApp/entities/Note.java
@@ -1,18 +1,11 @@
 package ua.gaponov.todoApp.entities;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import lombok.Data;
 
 @Data
 public class Note {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
-
   private String title;
-
   private String content;
 }
